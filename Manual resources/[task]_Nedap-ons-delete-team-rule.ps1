682c77bc3b207948006c9fd9c3fc44263c1fb4f9
Jerry Breek <j.breek@tools4ever.com>
2021-09-28 18:48:03 +0200

    Initial commit

---
+++ Manual resources/[task]_Nedap-ons-delete-team-rule.ps1
@@ -0,0 +1,11 @@
+$Path = $NedapOnsTeamsMappingPath
+
+$CSV = import-csv $Path -Delimiter ";"
+
+$filteredCSV = foreach($line in $CSV){
+    if(-not(($line.'Department.ExternalId' -eq $afasOrgUnits) -and ($line.NedapTeamId -eq $nedapTeamIds) -and ($line.'Title.ExternalId' -eq $functionIds))){
+        $line 
+    }
+}
+
+$filteredCSV | ConvertTo-Csv -NoTypeInformation -Delimiter ";" | % {$_.Replace('"','')} | Out-File $Path
